47280b03171db94a51f053ef355453558e3e0c4a
Dor cohen <78268224+plutus777@users.noreply.github.com>
2023-08-10 13:11:06 +0300

    ai-helper page info update

--- frontend/src/cmps/ai-helper.jsx
+++ frontend/src/cmps/ai-helper.jsx
@@ -1,7 +1,10 @@
+// icons
+import { LiaRobotSolid } from "react-icons/lia";
+
 export function AiHelper(){
     return (
         <section className="ai-strategy-creator-container">
-            <h2>Create trading strategy using AI, Coming soon...</h2>
+            <h2>Create trading strategy using AI <LiaRobotSolid/> , Coming soon... </h2>
         </section>
     )
 }
