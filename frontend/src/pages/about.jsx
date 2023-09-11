61aef5a0e5744259d6a41f29b6a7ef29ca41625a
Dor cohen <78268224+plutus777@users.noreply.github.com>
2023-09-11 11:09:23 +0300

    added about us page info

--- frontend/src/pages/about.jsx
+++ frontend/src/pages/about.jsx
@@ -1,7 +1,15 @@
-export function AboutUs(){
-    return (
-        <section className="about-us-main-container">
-            <h2>Coming soon...</h2>
-        </section>
-    )
+export function AboutUs() {
+   return (
+      <section className='about-us-main-container'>
+         <div className="about-us-container">
+            <h2 className="underline-style-white">About TradeSmart</h2>
+            <h3>
+               TradeSmart was built to help people who trade in the stock
+               market, as a trader I always lacked to follow transactions and
+               strategies and find insights, that's why I built TradeSmart,
+               which helps every trader to be better.
+            </h3>
+         </div>
+      </section>
+   )
 }
