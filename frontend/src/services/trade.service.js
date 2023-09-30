3585676410c10857806263a6232e60ce52d7a257
Dor cohen <78268224+plutus777@users.noreply.github.com>
2023-09-30 09:43:04 +0300

    refactor http trade.service file

--- frontend/src/services/trade.service.js
+++ frontend/src/services/trade.service.js
@@ -13,26 +13,273 @@ export const TradeService = {
 }
 window.cs = TradeService
 
-async function query(filterBy = { txt: '', price: 0 }) {
+async function query(filterBy = { symbol: '' }) {
    return httpService.get(STORAGE_KEY, filterBy)
+   // TODO filter should be handle in the backend (node server)
 }
 
-function getById(carId) {
-   return httpService.get(`car/${carId}`)
+function getById(tradeId) {
+   return httpService.get(`trade/${tradeId}`)
+   // TODO backend should return trade by his Id 
 }
 
-async function remove(carId) {
-   return httpService.delete(`car/${carId}`)
+async function remove(tradeId) {
+   return httpService.delete(`trade/${tradeId}`)
+   // TODO backend should remove trade from database
 }
-async function save(car) {
-   var savedCar
-   if (car._id) {
-      savedCar = await httpService.put(`car/${car._id}`, car)
+
+async function save(trade) {
+   var savedTrade
+   if (trade._id) {
+      savedTrade = await httpService.put(`trade/${trade._id}`, trade) // if trade has Id so update
    } else {
-      savedCar = await httpService.post('car', car)
+      savedTrade = await httpService.post('trade', trade) // if trade does not has id create add new trade 
    }
-   return savedCar
+   return savedTrade
 }
+
+
+
+// ## Functions that not require http request ## //
+
+
+// getting all the trades by the specific user
+async function getTrades(userId, storeTrades) {
+   if (!userId) return 'userId require'
+   const filterTradesByUser = storeTrades.filter(
+      (trade) => trade.owner._id === userId
+   )
+
+   return filterTradesByUser
+}
+
+async function filterStrategyByUser(userId) {
+   const loadStrategy = utilService.loadFromStorage(STRATEGY_KEY)
+   const filterStrategy = loadStrategy.filter((str) => str.owner._id === userId)
+   return filterStrategy
+}
+
+async function saveNewStrategy(newStrategy) {
+   newStrategy.owner = userService.getLoggedinUser()
+   const saveStrategy = await storageService.post(STRATEGY_KEY, newStrategy)
+   return saveStrategy
+}
+
+function calculatePL(symbol, sharesAmount, entryPrice, exitPrice) {
+   if (!exitPrice) return null
+   const totalCost = sharesAmount * entryPrice
+   const totalRevenue = sharesAmount * exitPrice
+   const profitLoss = totalRevenue - totalCost
+   const fixedProfitLoss = Math.round(profitLoss * 100) / 100
+   return fixedProfitLoss
+}
+
+// calculate percentage change
+function calculatePercentageChange(entryPrice, exitPrice) {
+   if (!exitPrice) return null
+
+   const percentageChange = ((exitPrice - entryPrice) / entryPrice) * 100
+   const fixedPercentageChange = Math.round(percentageChange * 100) / 100
+
+   return fixedPercentageChange
+}
+
+function calculateRiskAmount(accountValue, riskPercentage) {
+   console.log('accountValue', accountValue)
+   console.log('riskPercentage', riskPercentage)
+   var riskAmount = (accountValue * riskPercentage) / 100
+   return riskAmount
+}
+
+function calculatePercentageAndPrice(riskAmount, sharesAmount, sharesPrice) {
+   console.log('riskAmount', riskAmount)
+   console.log('sharesAmount', sharesAmount)
+   console.log('sharesPrice', sharesPrice)
+
+   // Calculate the total cost of the investment
+   const totalCost = sharesAmount * sharesPrice
+
+   // Calculate the breakeven point
+   const breakevenPoint = totalCost - riskAmount
+
+   // Calculate the new stock price required for the breakeven point
+   const newPricePerShare = breakevenPoint / sharesAmount
+
+   // Calculate the percentage decrease
+   const percentageDecrease =
+      ((sharesPrice - newPricePerShare) / sharesPrice) * 100
+
+   // Return the percentage decrease and the new stock price
+   return {
+      percentageDecrease: percentageDecrease.toFixed(2),
+      newPricePerShare: newPricePerShare.toFixed(2),
+   }
+}
+
+function emptyTrade() {
+   const newTrade = {
+      symbol: '',
+      amount: 0,
+      entryPrice: 0,
+      exitPrice: 0,
+      day: '',
+      pl: 0,
+      timestamp: null,
+      strategyType: null,
+   }
+   return newTrade
+}
+
+function newRiskManagement() {
+   const newRiskManagement = {
+      size: 0,
+      percentage: 0,
+   }
+   return newRiskManagement
+}
+
+function newCalcExitPoint() {
+   const newCalc = {
+      tradeRisk: 0,
+      sharesAmount: 0,
+      riskEntryPrice: 0,
+   }
+   return newCalc
+}
+
+// calculation functions for dashboard
+
+function calculateAccountPl(userTrades) {
+   const plArr = userTrades.map((trade) => trade.pl) // getting array
+   const userPl = plArr.reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+      0
+   )
+   return userPl
+}
+
+function calculateWinRate(userTrades) {
+   const totalTrades = userTrades.length
+   if (totalTrades === 0) {
+      return 0
+   }
+
+   const winningTrades = userTrades.filter((trade) => trade.pl > 0)
+   const winRate = (winningTrades.length / totalTrades) * 100
+   return winRate.toFixed(2)
+}
+// calculate strategy win rate
+function calcStrategyWinRate(userTrades) {
+   const countStrategy = userTrades.reduce((acc, trade) => {
+      if (trade.pl > 0) {
+         if (acc[trade.strategyType]) {
+            acc[trade.strategyType].winCount += 1
+         } else {
+            acc[trade.strategyType] = {
+               winCount: 1,
+               totalTrades: 0,
+            }
+         }
+      }
+
+      if (acc[trade.strategyType]) {
+         acc[trade.strategyType].totalTrades += 1
+      } else {
+         acc[trade.strategyType] = {
+            winCount: 0,
+            totalTrades: 1,
+         }
+      }
+
+      return acc
+   }, {})
+
+   const winRateResults = []
+
+   for (const strategyType in countStrategy) {
+      const winCount = countStrategy[strategyType].winCount
+      const totalTrades = countStrategy[strategyType].totalTrades
+      const winRate = (winCount / totalTrades) * 100
+
+      const resultObj = {
+         strategyType: strategyType,
+         winRate: parseFloat(winRate.toFixed(2)),
+      }
+
+      winRateResults.push(resultObj)
+   }
+   return winRateResults
+}
+
+function accountTradesAveragePercentage(userTrades) {
+   const tradesAveragePerChange = userTrades.reduce((acc, val) => {
+      console.log('acc', acc)
+      console.log('val', val.percentage)
+
+      return acc + val.percentage
+   }, 0)
+
+   const calcAverage = tradesAveragePerChange / userTrades.length
+   return calcAverage
+}
+
+// home page functions
+function createDemoUser() {
+   const demoUser = {
+      fullname: 'demo',
+      imgUrl:
+         'https://www.talpiot.ac.il/wp-content/themes/matat-child/assets/images/12.png',
+      username: 'demoUser',
+      password: '123',
+      _id: utilService.makeId(),
+   }
+
+   return demoUser
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ---------------------------------------------------------------- // 
 
 // async function addCarMsg(carId, txt) {
 //     const savedMsg = await httpService.post(`car/${carId}/msg`, {txt})
