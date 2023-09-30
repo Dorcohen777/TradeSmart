6458e8a54815777af09aa608c4f12f4748ac780d
Dor cohen <78268224+plutus777@users.noreply.github.com>
2023-09-30 09:15:03 +0300

    delete .env file, changing trade.service for http request

--- frontend/src/services/trade.service.js
+++ frontend/src/services/trade.service.js
@@ -1,59 +1,47 @@
-
 // import { storageService } from './async-storage.service.js'
 import { httpService } from './http.service.js'
 import { utilService } from './util.service.js'
 import { userService } from './user.service.js'
 
+const STORAGE_KEY = 'trade'
 
-const STORAGE_KEY = 'car'
-
-export const carService = {
-    query,
-    getById,
-    save,
-    remove,
-    getEmptyCar,
-    addCarMsg
+export const TradeService = {
+   query,
+   getById,
+   save,
+   remove,
 }
-window.cs = carService
-
+window.cs = TradeService
 
 async function query(filterBy = { txt: '', price: 0 }) {
-    return httpService.get(STORAGE_KEY, filterBy)
+   return httpService.get(STORAGE_KEY, filterBy)
 }
 
 function getById(carId) {
-    return httpService.get(`car/${carId}`)
+   return httpService.get(`car/${carId}`)
 }
 
 async function remove(carId) {
-    return httpService.delete(`car/${carId}`)
+   return httpService.delete(`car/${carId}`)
 }
 async function save(car) {
-    var savedCar
-    if (car._id) {
-        savedCar = await httpService.put(`car/${car._id}`, car)
-
-    } else {
-        savedCar = await httpService.post('car', car)
-    }
-    return savedCar
+   var savedCar
+   if (car._id) {
+      savedCar = await httpService.put(`car/${car._id}`, car)
+   } else {
+      savedCar = await httpService.post('car', car)
+   }
+   return savedCar
 }
 
-async function addCarMsg(carId, txt) {
-    const savedMsg = await httpService.post(`car/${carId}/msg`, {txt})
-    return savedMsg
-}
+// async function addCarMsg(carId, txt) {
+//     const savedMsg = await httpService.post(`car/${carId}/msg`, {txt})
+//     return savedMsg
+// }
 
-
-function getEmptyCar() {
-    return {
-        vendor: 'Susita-' + (Date.now() % 1000),
-        price: utilService.getRandomIntInclusive(1000, 9000),
-    }
-}
-
-
-
-
-
+// function getEmptyCar() {
+//     return {
+//         vendor: 'Susita-' + (Date.now() % 1000),
+//         price: utilService.getRandomIntInclusive(1000, 9000),
+//     }
+// }
