f22e6f49a0db3432d968dffc1b1f71814c30ed59
Dor cohen <78268224+plutus777@users.noreply.github.com>
2023-11-05 11:05:40 +0200

    change from local storage to mongoDB. creating new user

--- backend/api/user/user.routes.mjs
+++ backend/api/user/user.routes.mjs
@@ -1,11 +1,12 @@
 import express from 'express'
 import { requireAuth, requireAdmin } from '../../middlewares/requireAuth.middleware.mjs'
-import { getUser, getUsers, deleteUser, updateUser } from './user.controller.mjs'
+import { getUser, getUsers, deleteUser, updateUser, addUser } from './user.controller.mjs'
 
 const router = express.Router()
 
 router.get('/', getUsers)
 router.get('/:id', getUser)
+router.post('auth/signup', addUser)
 router.put('/:id', requireAuth, updateUser)
 router.delete('/:id', requireAuth, requireAdmin, deleteUser)
 
